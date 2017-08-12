af135c4f459fd71abbe1b6375e4e9527d16f0244
Vishesh <visheshsingh16@gmail.com>
2017-08-12 15:04:09 -0400

    Setting initial values when app loads up first time

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,19 +14,27 @@ export class HomePage {
         state: string
     }
 
-  constructor(public navCtrl: NavController, private WP: WeatherProvider) {
+  constructor(public navCtrl: NavController,
+            private WP: WeatherProvider,
+            private ST: Storage) {
 
   }
 
   ionViewWillEnter(){
-      this.location={
-          city: 'Miami',
-          state: 'FL'
-      }
+      this.ST.get('location').then((val) => {
+         if(val != null){
+             this.location = JSON.parse(val);
+         } else {
+             this.location={
+                 city: 'Princeton',
+                 state: 'NJ'
+             }
+         }
 
-      this.WP.getWeather(this.location.city, this.location.state)
-      .subscribe(weather => {
-          this.weather = weather.current_observation;
+         this.WP.getWeather(this.location.city, this.location.state)
+         .subscribe(weather => {
+             this.weather = weather.current_observation;
+         });
       });
   }
 
